4cc7ba64340da672636a801cab39595ec797a952
Maksim Bashchuk <MaksimBashchuk@gmail.com>
2021-11-12 01:47:39 +0300

    feat: add constants file

---
+++ constants.js
@@ -0,0 +1,26 @@
+module.exports.config = {
+  shortFlag: '-c',
+  longFlag: '--config',
+  name: 'config',
+};
+
+module.exports.input = {
+  shortFlag: '-i',
+  longFlag: '--input',
+  name: 'input',
+};
+
+module.exports.output = {
+  shortFlag: '-o',
+  longFlag: '--output',
+  name: 'output',
+};
+
+module.exports.optionsArr = [
+  '-c',
+  '--config',
+  '-i',
+  '--input',
+  '-o',
+  '--output',
+];
